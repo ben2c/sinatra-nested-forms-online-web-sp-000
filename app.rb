a92c656fc15b08e92e222799dee1087f697b1908
Benjamin Cheng <bencheng200@gmail.com>
2021-01-06 17:35:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ module FormsLab
     post '/pirates' do
       @pirates = Pirate.new(params[:pirate])
 
-      params[:pirates][:ships].each do |ship|
+      params[:pirate][:ships].each do |ship|
         Ship.new(ship)
       end
 
